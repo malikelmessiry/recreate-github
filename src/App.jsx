07c4d1143355dc056c2f7396f56e79530750b4e1
Malik Elmessiry <malik.messiry@gmail.com>
2025-06-06 14:43:10 +0200

    add nav bar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import './App.css';
 import NavigationBar from './components/NavigationBar';
 
 function App() {
 
   return (
-    <main>
-      <NavigationBar></NavigationBar>
-    </main>
+    <div className="app-background">
+      <main>
+        <NavigationBar></NavigationBar>
+      </main>
+    </div>
   );
 };
 
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -3,8 +3,9 @@ import './navigation-bar.css';
 const NavigationBar = () => {
     return (
         <nav>
-            <img></img>
+            
             <ul>
+                <li><img src="./src/assets/github-mark-white.png" className="logo"></img></li>
                 <li>Why Github? <i className="arrow"></i></li>
                 <li>Team</li>
                 <li>Enterprise</li>
